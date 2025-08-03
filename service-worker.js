0cae2c9ac09e8077fb6417c947a8f65d360629c3
Aldair Zapata <alzpta@gmail.com>
2025-08-03 14:05:25 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Service Worker para Carta Nomo (scope: /Carta-Nomo/) */
-const VERSION = 'v1.1.0';
+const VERSION = 'v1.0.0';
 const CACHE_NAME = `carta-nomo-${VERSION}`;
 const APP_SHELL = [
   '/Carta-Nomo/',
